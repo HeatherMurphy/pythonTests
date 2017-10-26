1b5a6cca5b3e59d62288813d8199c126adad1ee9
HeatherMurphy <heathermurphy11011@gmail.com>
2017-10-26 18:31:28 -0400

    first function
    Very simple code.

---
+++ converter function.py
@@ -0,0 +1,9 @@
+#Heather Murphy
+#Miles to Kilometers
+#10/26/17
+def converter ():
+    Kilos = float(input ('How many kilometers have you travelled?: '))
+    Miles = Kilos * .6214
+    print (Miles, 'miles have been travelled!!')
+converter()
+
